b2a6091a483a5f2abf4c174722cec9604fb85b80
Emir Mekić <emir.mekic@live.com>
2019-02-14 21:10:53 +0100

    Updates

---
+++ precache-manifest.952e51dda0af8fe1840be50853fa769b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4392c70779683333373e",
+    "url": "/moviesTVreact/static/js/runtime~main.4392c707.js"
+  },
+  {
+    "revision": "8d56a891504b1dfdcff6",
+    "url": "/moviesTVreact/static/js/main.8d56a891.chunk.js"
+  },
+  {
+    "revision": "0a41f8da99db56fce3df",
+    "url": "/moviesTVreact/static/js/1.0a41f8da.chunk.js"
+  },
+  {
+    "revision": "8d56a891504b1dfdcff6",
+    "url": "/moviesTVreact/static/css/main.d2d9e85d.chunk.css"
+  },
+  {
+    "revision": "0a41f8da99db56fce3df",
+    "url": "/moviesTVreact/static/css/1.c55daac0.chunk.css"
+  },
+  {
+    "revision": "838de405c5577535ab1f487749ff26a8",
+    "url": "/moviesTVreact/index.html"
+  }
+];
