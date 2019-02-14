84b948cd05ff99bac04f2b276b83d5efc63368b8
Emir Mekić <emir.mekic@live.com>
2019-02-14 22:19:10 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import Movies from "./components/Movies";
 import Details from "./components/Details";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path='/' component={Movies} exact />
           <Route path='/details' component={Details} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
