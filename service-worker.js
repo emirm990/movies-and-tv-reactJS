3d3b8d8ba8359b5dc0b7663a8da2d93473e5c16f
Emir Mekić <emir.mekic@live.com>
2019-02-14 21:17:47 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/moviesTVreact/precache-manifest.952e51dda0af8fe1840be50853fa769b.js"
+  "/movies-and-tv-reactJS/precache-manifest.4253a7f535ffaae13506c5c55c7669b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/moviesTVreact/index.html", {
+workbox.routing.registerNavigationRoute("/movies-and-tv-reactJS/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
